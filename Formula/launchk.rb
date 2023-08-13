0b671b1a351b2a69c84c6883d898f2cc5cc64d7e
David Stancu <dstancu@nyu.edu>
2023-08-13 17:28:31 -0400

    fix tag

--- Formula/launchk.rb
+++ Formula/launchk.rb
@@ -11,12 +11,12 @@ class Launchk < Formula
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/mach-kernel/launchk/releases/download/#{version}/launchk-x86_64-apple-darwin.zip"
+      url "https://github.com/mach-kernel/launchk/releases/download/launchk-#{version}/launchk-x86_64-apple-darwin.zip"
       sha256 "481ea6d336fc2033ab1489435a5bf7fe8b58eaafdeb50d27c8df8b68e7c433f8"
     end
 
     if Hardware::CPU.arm?
-      url "https://github.com/mach-kernel/launchk/releases/download/#{version}/launchk-aarch64-apple-darwin.zip"
+      url "https://github.com/mach-kernel/launchk/releases/download/launchk-#{version}/launchk-aarch64-apple-darwin.zip"
       sha256 "61180ed6429c5a35c4d8de4bff249fb88fcb634c8a5a2adc3b23146e20e965ce"
     end
   end
