2a4c59bfa6f596961045bee4e9bef0dba88f8b62
David Stancu <dstancu@nyu.edu>
2025-06-07 15:29:18 -0400

    launchk 0.3.1

--- Formula/launchk.rb
+++ Formula/launchk.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 class Launchk < Formula
-  version '0.3.0'
+  version '0.3.1'
   desc 'Rust Cursive TUI that helps manage launchd jobs on macOS'
   homepage 'https://github.com/mach-kernel/launchk'
-  url "https://github.com/mach-kernel/launchk/archive/refs/tags/launchk-0.3.0.tar.gz"
+  url "https://github.com/mach-kernel/launchk/archive/refs/tags/launchk-#{version}.tar.gz"
 
   uses_from_macos 'ncurses'
   uses_from_macos 'libiconv'
@@ -12,12 +12,12 @@ class Launchk < Formula
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/mach-kernel/launchk/releases/download/launchk-#{version}/launchk-x86_64-apple-darwin.tar.gz"
-      sha256 "095a91f841a30937b61a72de8648c39ae9843c5dd6b5efa4077039766114e745"
+      sha256 "a62171db8b6bdc937c286bab73ba49efe5ddb41b302325f4320cb843b9c0c944"
     end
 
     if Hardware::CPU.arm?
       url "https://github.com/mach-kernel/launchk/releases/download/launchk-#{version}/launchk-aarch64-apple-darwin.tar.gz"
-      sha256 "078f14a86fcc023c43609d32be66990265557015bf154b1614bdc15deb315760"
+      sha256 "3752298ceb058160a413bd041e765750030019f9513f2c2b9520ccb08da4f93f"
     end
   end
 
